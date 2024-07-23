0730ef0d4200e18aa91916c16f4842001c17d3db
Craig Richards <craiganthonyrichards@gmail.com>
2024-07-23 10:41:06 +1000

    Updated code

---
+++ emotion_detection.py
@@ -0,0 +1,9 @@
+import requests
+
+def emotion_detector(text_to_analyse: str):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json=myobj, headers=header, timeout=120)
+    
+    return response.text
